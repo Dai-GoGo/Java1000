fd6dc4eca034f84f29fe313b73db460e5a180fc9
Dai-GoGo <CoryChen2020@gmail.com>
2022-07-22 15:27:43 +0800

    给方法增加注释做备注

--- src/main/java/com/javabasic/tool/ToUpperCaseEmp.java
+++ src/main/java/com/javabasic/tool/ToUpperCaseEmp.java
@@ -3,7 +3,7 @@ package com.javabasic.tool;
 public class ToUpperCaseEmp {
     //字符串大小写转换的方法
     public static void main(String[] args){
-        String str = "hello world";
+        String str = "string hello world";
         String strUpper = str.toUpperCase();
         System.out.println("原始字符串："+str);
         System.out.println("转化为大写："+strUpper);
